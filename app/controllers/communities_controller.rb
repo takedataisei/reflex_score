b080692bdab3447bb0cfd9af3ea7b251b3b9947f
takedataisei <tsukuda120835@gmail.com>
2024-06-07 17:58:26 +0900

    コミュニティの作成画面を実装

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,4 +1,12 @@
 class CommunitiesController < ApplicationController
   def index
   end
+
+  def new
+    @community = Community.new
+  end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "communities#index"
   resources :users, only: [:show, :edit, :update]
+  resources :communities, only: [:new, :create]
 end
