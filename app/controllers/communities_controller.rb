36b4f17c321bcc89972c6115109427dabdf6a3f7
takedataisei <tsukuda120835@gmail.com>
2024-06-08 22:45:29 +0900

    コミュニティ参加機能を実装

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -15,8 +15,34 @@ class CommunitiesController < ApplicationController
     end
   end
 
+  def join
+    @community = Community.new
+  end
+
+  def process_join
+    @community = Community.find_by(name: params[:name])
+
+    if @community && @community.authenticate(params[:password])
+      if @community.users.include?(current_user)
+        flash.now[:alert] = 'すでにこのコミュニティに参加しています'
+        render_flash
+      else
+        CommunityMembership.create(user_id: current_user.id, community_id: @community.id, role: :member)
+        redirect_to community_path(@community.id)
+      end
+    else
+      flash.now[:alert] = 'コミュニティ名またはパスワードが正しくありません'
+      render_flash
+    end
+  end
+
   private
   def community_params
     params.require(:community).permit(:name, :password, :password_confirmation, :description, user_ids:[])
   end
+
+  def render_flash
+    render turbo_stream: turbo_stream.update("flash", partial: "shared/flash")
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "communities#index"
   resources :users, only: [:show, :edit, :update]
+  get 'communities/join', to: 'communities#join', as: 'join_community'
+  post 'communities/join', to: 'communities#process_join'
   resources :communities, only: [:new, :create, :show]
 end
