a5d0d73d4bcf38d6444afe95b183b29f72885f35
takedataisei <tsukuda120835@gmail.com>
2024-06-14 14:25:09 +0900

    評価一覧の画面の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
   before_action :set_user
   before_action :authenticate_user!
   before_action :move_to_root, only: :edit
-  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+  before_action :set_community, only: :evaluations
+  before_action :check_membership, only: :evaluations
 
   def show
   end
@@ -17,6 +19,13 @@ class UsersController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def evaluations
+    @evaluation_items = @community.evaluation_items.includes(:self_evaluations, :peer_evaluations)
+    @self_evaluations = @user.self_evaluations.where(evaluation_item: @evaluation_items).order(created_at: :desc)
+    @peer_evaluations_received = @user.evaluations_received.where(evaluation_item: @evaluation_items).order(created_at: :desc)
+  end
+
   private
   def user_params
     params.require(:user).permit(:image, :username, :profile)
@@ -35,5 +44,16 @@ class UsersController < ApplicationController
   def user_not_found
     flash[:alert] = "存在しないユーザーです。"
     redirect_to root_path
+  end
+
+  def set_community
+    @community = Community.find(params[:community_id])
+  end
+
+  def check_membership
+    unless @community.users.include?(current_user)
+      flash[:alert] = 'このコミュニティに参加していません'
+      redirect_to root_path
+    end
   end
 end
