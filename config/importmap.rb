9adb13ef3e50469e4b2a1632da1363cfca594886
takedataisei <tsukuda120835@gmail.com>
2024-06-23 17:48:58 +0900

    JavaScriptの読み込みエラーの修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,6 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@rails/ujs", to: "rails-ujs.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
@@ -10,3 +9,4 @@ pin 'communities', to: 'communities.js'
 pin 'star', to: 'star.js'
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+pin "@rails/ujs", to: "rails-ujs.js"
